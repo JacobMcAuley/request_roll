dd20f9f41c0fe1bd68a1965ee5406641ea79eb68
Jacob McAuley <mcauleyj@uci.edu>
2019-12-22 18:07:00 -0800

    Handle conversions for roller

--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ class RequestRoll extends FormApplication {
     constructor(...args){
         super(...args);
         this.SOCKET = "module.request_roll"
-        this.playerReference = {};
-        this._initSocket();
+        this._showDM = false;
         this._selected = [];
         this._attributes = [];
         this._saves = [];
         this._skills = [];
-        
+        this.commonUsage = {
+            pos : ["+2", "+5"],
+            neg : ["-2", "-5"]
+        }
+
+        this.active = false;   
     }
 
     static get defaultOptions() {
@@ -24,7 +28,7 @@ class RequestRoll extends FormApplication {
 
     async getData() {
         const templateData = {
-            items: await this._setPortraits(),
+            items: await this.setPortraits(),
             attribute: CONFIG.DND5E["abilities"],
             skill: CONFIG.DND5E["skills"]
         }
@@ -32,15 +36,148 @@ class RequestRoll extends FormApplication {
         return templateData;
     }
  
-    _updateObject(event, formData) {
-        let userLookup = ev.currentTarget.getAttribute('src')
-
-        game.socket.emit("module.request_roll", this.playerReference[userLookup], resp => {
-            console.log("SENT");
-        });
-        console.log(formData);
+    sendPackets(packets){
+        packets.forEach(packet => {
+            game.socket.emit("module.request_roll", packet, resp => { // Adjust playerRef
+                console.log("SENT");
+            });
+        })
     }
 
+    _updateObject(event, formData) {
+        let sortedData = this._sortData(formData);
+        let packetsToBeSent = this._seperatePackets(sortedData);
+        this.sendPackets(packetsToBeSent);
+    }
+
+    _seperatePackets(sortedData){
+        let players = this._determineNecessaryPlayers(sortedData);
+        let data = this._determineNecessaryOther(sortedData);
+        let modifiers = this._determineNecessaryModifiers(sortedData);
+        return this._generatePacket(players, data, modifiers)
+    }
+
+    _generatePacket(players, data, modifiers){
+        let listofPackets = []
+        for(const [key, value] of Object.entries(players)){
+            if(value.length != 0)
+            {
+                listofPackets.push({
+                    userId: key,
+                    characters: value,
+                    attributes: data.attributes,
+                    skills: data.skills,
+                    saves: data.saves,
+                    modifiers: modifiers
+                });
+            }
+
+        }
+        return listofPackets;
+    }
+
+    _determineNecessaryModifiers(data){
+        let packet = {'mod': 0, 'bonus': 0, 'dc': 0, 'advantage': 0, 'hidden': data.modifiers.hidden}
+        const numberConverter = {
+            'one' : 0,
+            'two' : 1
+        }
+        for(let [key, value] of Object.entries(data.modifiers)){
+            if(key.includes('common') && value == true){
+                let tempKey = key;
+                tempKey = tempKey.replace("common-", "");
+                let index = numberConverter[tempKey.substring(0,3)];
+                let sign = tempKey.substring(tempKey.length-3, tempKey.length);
+                let result = parseInt(this.commonUsage[sign][index]);
+                packet['bonus'] += result;
+            }
+            else if(key == "advantage" && value != false){
+                packet[key] += 1;
+            }
+            else if(key == "disadvantage" && value != false){
+                packet['advantage'] += -1;
+            }
+            else if(key == "bonus"){
+                    if(isNaN(value)){
+                        ui.notifications.error("Roll Request: Invalid input in 'Modifiers' section. Defaulting to 0");
+                    }
+                    value = parseInt(value) || 0;
+                    packet[key] += parseInt(value);
+            }
+            else if(key == "dc"){
+                if(isNaN(value)){
+                    ui.notifications.error("Roll Request: Invalid input in 'dc' section. Defaulting to 10");
+                }
+                value = parseInt(value) || 10;
+                packet[key] += parseInt(value);
+            } 
+        }
+        return packet;
+    }
+
+
+    _determineNecessaryPlayers(data){
+        let packet = {};
+        for(const [key, value] of Object.entries(data.players)){
+            if(value == true){
+                let parts = key.split('.');
+                if(packet[parts[0]] == undefined){
+                    packet[parts[0]] = [];
+                }
+                packet[parts[0]].push(parts[1]);
+            }
+        }  
+        return packet;
+    }
+
+
+
+    _determineNecessaryOther(data){
+        let packet = {'attributes': [], "skills": [], "saves": []}
+        for(const [parentKey, parentValue] of Object.entries(data)){
+            if(parentKey != "players" && parentKey != "modifiers"){
+                for(let [key, value] of Object.entries(parentValue)){
+                    if(value != false){
+                        packet[parentKey].push(key);
+                    }
+                }
+            } 
+        }
+        return packet;
+    }
+
+    _sortData(formData){
+        let players = this._filter(formData, "player");
+        let attributes = this._filter(formData, "attribute");
+        let skills = this._filter(formData, "skill");
+        let saves = this._filter(formData, "save");
+        let modifiers = this._filter(formData, "modifier");
+        return {
+            "players": players,
+            "attributes": attributes,
+            "skills": skills,
+            "saves" : saves,
+            "modifiers":  modifiers
+        };
+    }
+
+    _filter(formData, wordReq){
+        let dict = {};
+        let keys = Object.keys(formData).filter(word => word.includes(wordReq) == true);
+        keys.forEach(key => {
+            if(wordReq == "player" || wordReq == "modifier")
+                dict[key.replace((wordReq+"-"), "")] = formData[key];
+            else{
+                if(key.includes("Performance"))
+                    dict['prf'] = formData[key];
+                else if( key.includes("Perception"))
+                    dict['prc'] = formData[key];
+                else
+                    dict[(key.replace((wordReq+"-"), "")).substring(0,3).toLowerCase()] = formData[key];
+            }
+        })
+        return dict;
+    }
 
     activateListeners(html) {
         super.activateListeners(html);
@@ -56,38 +193,63 @@ class RequestRoll extends FormApplication {
     }
 
 
-    async _setPortraits(){
+    async setPortraits(){
+        let userSet = this._determineUserSize();
+        let informationField = this._determineOwnedOrActive(userSet);
+        return informationField;
+    }
+
+    _determineUserSize(){
         let users = game.users.entities
         let userSet = [];
-        let imageArray = []
         users.forEach(user => {
-            if(!user.isGM)
+            if(!user.isGM || this._showDM)
                 userSet.push(user)
         });
+        return userSet;
+    }
 
+    _determineOwnedOrActive(userSet){
+        if(this.active){
+            return this._active(userSet);
+        }
+        else{
+            return this._owned(userSet);
+        }
+    }
+
+    _active(userSet){
+        let informationField = []
         userSet.forEach(async user => {
             let image = await user.character.getTokenImages(); 
-            imageArray.push(image);
-            this.playerReference[image] = user.id;
+            informationField.push({
+                img: image, 
+                id: user.id +'.' + user.character.id
+            });
         });
-
-        return imageArray;
+        return informationField;
     }
 
-    _initSocket(){
-        game.socket.on("module.request_roll", user => {
-            if(game.user.id == user){
-                //Handle Other Application
-            }
+    _owned(userSet){
+        let informationField = []
+        userSet.forEach(async user => {
+            let actors = game.actors.entities.filter(actor => actor.hasPerm(user, "OWNER"))
+            actors.forEach(async actor => {
+                let image = await actor.getTokenImages(); 
+                informationField.push({
+                    img: image, 
+                    id: user.id + '.' + actor.id
+                });
+            });
         });
+        return informationField;
     }
-
-
-
 }
 
 Hooks.on('ready', ()=>{
-    let ps = new RequestRoll();
-    ps.render(true);
+    if(game.user.isGM){
+        let ps = new RequestRoll();
+        ps.render(true);
+    }
 })
 
